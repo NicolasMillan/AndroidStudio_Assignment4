492ce81e12f5eb8d070343f43d1ba708282d0fbe
NicolasMillan <134756328+NicolasMillan@users.noreply.github.com>
2024-07-17 13:23:47 -0400

    finishing layout

--- app/src/main/java/ca/georgiancollege/assignmentthree/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/assignmentthree/MainActivity.kt
@@ -5,13 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import ca.georgiancollege.assignmentthree.databinding.TodoListActivityBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: TodoListActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.todo_list_activity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Inflate the layout for this activity
+        binding = TodoListActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(binding.root.id)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
