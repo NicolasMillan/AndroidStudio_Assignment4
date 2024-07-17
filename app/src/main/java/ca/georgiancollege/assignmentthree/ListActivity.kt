18b1abf22e1ebd4ff6afc4750e81635290cb8406
NicolasMillan <134756328+NicolasMillan@users.noreply.github.com>
2024-07-17 14:40:26 -0400

    everything in the layout is working

--- app/src/main/java/ca/georgiancollege/assignmentthree/ListActivity.kt
+++ app/src/main/java/ca/georgiancollege/assignmentthree/ListActivity.kt
@@ -1,30 +1,32 @@
 package ca.georgiancollege.assignmentthree
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.recyclerview.widget.LinearLayoutManager
 import ca.georgiancollege.assignmentthree.databinding.TodoListActivityBinding
 
-class MainActivity : AppCompatActivity() {
+class ListActivity : AppCompatActivity() {
 
     private lateinit var binding: TodoListActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-
-        // Inflate the layout for this activity
+//        Create a reference to the ActivityMainBinding class object
         binding = TodoListActivityBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
         setContentView(binding.root)
-
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(binding.root.id)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        binding.todoListTitle.setOnClickListener {
+//            val intent = Intent(this, TodoDetailsActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
